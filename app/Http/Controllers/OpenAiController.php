c24b945d9ea079af776f7c51c3d393004a485c88
Mitesh Viras <mitesh.viras@appstonelab.com>
2023-07-19 18:32:39 +0530

    optimization required

--- app/Http/Controllers/OpenAiController.php
+++ app/Http/Controllers/OpenAiController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\OpenAIResponceCollection;
 use OpenAI;
-use App\Models\OpenAi as OpenAiModel;
+use Illuminate\Support\Arr;
 use App\Models\UserTransaction;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use App\Models\OpenAi as OpenAiModel;
+use App\Http\Resources\OpenAIResponceCollection;
 
 class OpenAiController extends Controller
 {
@@ -19,15 +20,29 @@ class OpenAiController extends Controller
 
     public function chat()
     {
-        // ['role' => 'user', 'content' => 'countries json encoded string given, fetch data from above encoded string.'],
+
         $model = 'gpt-3.5-turbo-16k';
         $response = $this->client->chat()->create([
             'model' => $model,
             'messages' => [
-                ['role' => 'system', 'content' => "Transaction Json Encoded Data : " . $this->getTransactions()],
-                ['role' => 'system', 'content' => 'return data as php array with tansaction ID, we call transaction_date as date '],
-                ['role' => 'user', 'content' => 'list all transactions date between 2022-09-15 to 2023-04-26.'],
+                ['role' => 'system', 'content' => 'you are data analyst expert & working as a data analyst over 10+ years.'],
+                ['role' => 'system', 'content' => "Transaction Json Encoded Data ``` " . $this->getTransactions()." ``` " ],
+                ['role' => 'user', 'content' => 'list of top 5 most spent amounts on category.'],
+                ['role' => 'user', 'content' => 'Todo : 1. return the output in json format'],
+                ['role' => 'user', 'content' => '<debit_credit_type describes the amount is spent or income. if requested debited transaction details convert its value to positive value.>'],
+                ['role' => 'user', 'content' => '<desire format : [{
+                    "id": <transaction id>
+                    "plain_name": <plain name>
+                    "transaction_type": <transaction type>
+                    "category": <category name>
+                    "credit_debit_type": <credit_debit_type>
+                    "transaction_date": <transaction date>
+                    "amount": <amount>
+                }]>'],
+                ['role' => 'user', 'content' => 'if any of the case is failed or not getting output return ``` {}``` empty json object'],
+                ['role' => 'user', 'content' => 'note. i want data in json format only.'],
             ]
+
         ]);
 
         OpenAiModel::create(['response' => json_encode($response), 'tokens' => $response->usage->totalTokens, 'model' => $model]);
@@ -36,8 +51,9 @@ class OpenAiController extends Controller
 
     public function getChats()
     {
+        dD(UserTransaction::get()->sum('geolocation_id') / 100000);
         return OpenAiModel::latest()->first()->response->choices[0]->message;
-        return OpenAiModel::orderBy('id','desc')->get()->pluck('response.choices')->toArray();
+        return OpenAiModel::orderBy('id', 'desc')->get()->pluck('response.choices')->toArray();
     }
 
     public function responseBreaker()
@@ -48,7 +64,7 @@ class OpenAiController extends Controller
 
     public function getTransactions(int $user_id = null)
     {
-        return UserTransaction::select('plain_updated_name as plain_name', 'transaction_type', 'primary_category_title as category', 'credit_debit_type', 'transaction_date')->selectRaw('geolocation_id / 100000 as amount')->get()->toJson();
+        return UserTransaction::select('id', 'plain_updated_name as plain_name', 'transaction_type', 'primary_category_title as category', 'credit_debit_type', 'transaction_date')->selectRaw('geolocation_id / 100000 as amount')->get()->take(25)->toJson();
     }
 
     public function getCountries()
